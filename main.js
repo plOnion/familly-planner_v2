d35523075f27fc98f775d2f1c7c37fc93c459766
plOnion <damian.lukasiewicz@gmail.com>
2019-02-11 22:42:28 +0100

    add close button to modal

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const monthHeader = [...document.querySelectorAll(".header__month")];
+const daysTable = [...document.querySelectorAll("#month .day")];
 const date = new Date();
 const months = ["styczeń", "luty", "marzec", "kwiecień", "maj", "czerwiec", "lipiec", "sierpień", "wrzesień", "październik", "listopad", "grudzień"];
 const days = ["niedziela", "poniedziałek", "wtorek", "środa", "czwartek", "piątek", "sobota"];
@@ -26,30 +27,34 @@ const today = () =>{
     const weekDays = [...document.querySelectorAll(".week .day")];
     if (day === 0) {day = 7}
         weekDays[day-1].classList.add("today");
+        daysTable[dayNumb+firstMonthDay-2].classList.add("today");
 }
 
 today();
 
 // ****************************** MODAL
-const dayTasks = [...document.querySelectorAll(".day__task")];
+// const dayTasks = [...document.querySelectorAll(".day__task")];
+
 const members = [...document.querySelectorAll(".member")];
 const modal = document.querySelector(".modal");
-const modalBtn = document.querySelector(".modal input[type=submit");
+const modalClose = document.querySelector(".modal input[type=reset");
 const modalInput = document.querySelector(".modal input[name=newtask");
 
 const newTask = () =>{
     let active = document.querySelector(".active");
-        active.textContent += " " + modalInput.value;
+        active.textContent = modalInput.value;
         closeModal();
-}
+    }
 
 const closeModal = () =>{
     modalInput.value = "";
     modal.style.visibility = "hidden";
+    let active = document.querySelector(".active");
+        active.classList.remove("active");
 }
 
 modalInput.addEventListener("change", newTask);
-modalBtn.addEventListener("click", closeModal);
+modalClose.addEventListener("click", closeModal);
 
 members.forEach((item) =>{
     item.addEventListener("click", (e)=>{
@@ -57,13 +62,15 @@ members.forEach((item) =>{
         e.preventDefault;
         modal.style.visibility = "visible";
     })
-    
 })
 
 // ****************
-const daysTable = [...document.querySelectorAll("#month .day")];
+
 let number = 1;
 for (let i=firstMonthDay-1; i<daysInMonth+firstMonthDay-1; i++){
     daysTable[i].dataset.number = number++;
-    daysTable[day-1].classList.add("today");
+    daysTable[i].innerHTML = `XXX<div class="square">${daysTable[i].dataset.number}<div>`;
+    daysTable[i].classList.contains("square") ? daysTable[i].innerHTML = `<div class='xxx'>z</div>` : number;
 }
+
+// ********************kopiowanie zdarzeń
